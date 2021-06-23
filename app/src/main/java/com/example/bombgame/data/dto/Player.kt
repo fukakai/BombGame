4374f70f66f157a4e3d43a4cd5dccb9724d4d2b6
Romain DALICHAMP <romain.dalichamp@alithya.com>
2021-06-23 19:55:25 -0400

    POC: Room and Playr models Update

---
+++ app/src/main/java/com/example/bombgame/data/dto/Player.kt
@@ -0,0 +1,3 @@
+package com.example.bombgame.data.dto
+
+data class Player (var username: String, var ready: Boolean = false)
--- app/src/main/java/com/example/bombgame/data/dto/Room.kt
+++ app/src/main/java/com/example/bombgame/data/dto/Room.kt
@@ -1,9 +1,11 @@
 package com.example.bombgame.data.dto
 
-data class Room(var gameId: String = "", var currentBombOwner: String = "Romain") {
-
-    override fun toString(): String {
-        return "Room $gameId"
-    }
-}
-
+data class Room(
+    var bombState: String = "",
+    var currentBombOwner: String = "Romain",
+    var deltaXCoef: Number = 7,
+    var deltaYCoef: Number = 7,
+    var endOfGame: Number = 0,
+    var timeFromBeginning: Number = 0,
+    var playerList: List<Player> = mutableListOf()
+)
