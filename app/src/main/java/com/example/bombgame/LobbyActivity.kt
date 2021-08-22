33787def3e355ada0c37e4d1bca64b798d7790be
Florian Rea <rea.florian@gmail.com>
2021-08-22 11:37:17 -0400

    Code cleaning

--- app/src/main/java/com/example/bombgame/LobbyActivity.kt
+++ app/src/main/java/com/example/bombgame/LobbyActivity.kt
@@ -3,14 +3,10 @@ package com.example.bombgame
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.bombgame.data.dto.Player
 import com.example.bombgame.models.Subscription
 import com.example.bombgame.ui.main.RoomViewModel
@@ -23,6 +19,7 @@ import com.example.bombgame.utils.Constants.RED
 import com.example.bombgame.utils.Constants.ROOM_ID_KEY
 import com.example.bombgame.utils.InjectorUtils
 import com.example.bombgame.utils.PlayersUtils
+import kotlinx.android.synthetic.main.activity_lobby.*
 
 
 class LobbyActivity : AppCompatActivity() {
@@ -32,25 +29,66 @@ class LobbyActivity : AppCompatActivity() {
     private lateinit var roomViewModel: RoomViewModel
     private var playerList: List<Player> = mutableListOf()
     private var ready = false
+    private var gameAlreadyStarted = false
+    private val playerListAdapter = LobbyAdapter(playerList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lobby)
+
+        players_list_view.adapter = playerListAdapter
+        players_list_view.layoutManager = GridLayoutManager(this, 2)
+
+        initializeExtras()
+        initializeButtons()
+        initializeRoomViewModel()
+
+        room_id.text = roomId
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        roomViewModel.deletePlayer(playerUsername, roomId)
+        roomViewModel.listenToRoomList()
+        roomViewModel.unsubscribe(listOf(Subscription.PLAYER_LIST, Subscription.ROOM))
+    }
+
+    /**
+     * Starts the game activity, set player ready to false.
+     */
+    private fun startGameActivity(playerId: String, gameId: String) {
+        roomViewModel.switchReady(roomId, playerUsername)
+        val intent = Intent(this, GameActivity::class.java)
+        intent.putExtra(PLAYER_USERNAME_KEY, playerId)
+        intent.putExtra(ROOM_ID_KEY, gameId)
+        startActivity(intent)
+    }
+
+    /**
+     * Initialize the buttons for the lobby activity.
+     */
+    private fun initializeButtons() {
+        ready_button.setBackgroundColor(Color.parseColor(RED))
+
+        start_button.setOnClickListener {
+            roomViewModel.updateStartedGame(roomId, true)
+        }
+
+        ready_button.setOnClickListener {
+            roomViewModel.switchReady(roomId, playerUsername)
+        }
+
+        leave_button.setOnClickListener {
+            finish()
+        }
+    }
+
+    /**
+     * Get the game ID and the local player's username from the main activity.
+     */
+    private fun initializeExtras() {
         val intentLobbyActivity = intent
         val bundle = intentLobbyActivity.extras
-
-        var gameAlreadyStarted = false
-        val playerListView = findViewById<RecyclerView>(R.id.players_list)
-        val playerListAdapter = LobbyAdapter(playerList)
-        val pseudo = findViewById<EditText>(R.id.pseudo_text)
-        val readyButton = findViewById<Button>(R.id.ready_button)
-        readyButton.setBackgroundColor(Color.parseColor(RED))
-        val leaveButton = findViewById<Button>(R.id.leave_button)
-        val startButton = findViewById<Button>(R.id.start_button)
-        val roomIdText = findViewById<TextView>(R.id.room_id)
-
-        playerListView.adapter = playerListAdapter
-        playerListView.layoutManager = GridLayoutManager(this, 2)
 
         if (bundle?.getString(ROOM_ID_KEY) != null) {
             roomId = bundle.getString(ROOM_ID_KEY)!!
@@ -59,9 +97,12 @@ class LobbyActivity : AppCompatActivity() {
         if (bundle?.getString(PLAYER_USERNAME_KEY) != null) {
             playerUsername = bundle.getString(PLAYER_USERNAME_KEY)!!
         }
+    }
 
-        roomIdText.text = roomId
-
+    /**
+     * Initialize the RoomViewModel and listen to the current room and the players list.
+     */
+    private fun initializeRoomViewModel() {
         val roomFactory = InjectorUtils.provideRoomViewModelFactory()
         roomViewModel = ViewModelProvider(this, roomFactory)
             .get(RoomViewModel::class.java)
@@ -81,38 +122,12 @@ class LobbyActivity : AppCompatActivity() {
                 ready = player.ready
             }
 
-            readyButton.setBackgroundColor(if (ready) Color.parseColor(GREEN) else Color.parseColor(
+            ready_button.setBackgroundColor(if (ready) Color.parseColor(GREEN) else Color.parseColor(
                 RED))
-            readyButton.text = if (ready) READY_TRUE else READY_FALSE
+            ready_button.text = if (ready) READY_TRUE else READY_FALSE
 
-            startButton.isEnabled = playerList.size > 1 && PlayersUtils.areAllPlayersReady(playerList)
+            start_button.isEnabled =
+                playerList.size > 1 && PlayersUtils.areAllPlayersReady(playerList)
         })
-
-        startButton.setOnClickListener {
-            roomViewModel.updateStartedGame(roomId, true)
-        }
-
-        readyButton.setOnClickListener {
-            roomViewModel.switchReady(roomId, playerUsername)
-        }
-
-        leaveButton.setOnClickListener {
-            finish()
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        roomViewModel.deletePlayer(playerUsername, roomId)
-        roomViewModel.listenToRoomList()
-        roomViewModel.unsubscribe(listOf(Subscription.PLAYER_LIST, Subscription.ROOM))
-    }
-
-    private fun startGameActivity(playerId: String, gameId: String) {
-        roomViewModel.switchReady(roomId, playerUsername)
-        val intent = Intent(this, GameActivity::class.java)
-        intent.putExtra(PLAYER_USERNAME_KEY, playerId)
-        intent.putExtra(ROOM_ID_KEY, gameId)
-        startActivity(intent)
     }
 }
